67a709fc738983e7afac6b593f3f775dac9be5bb
secretsanta-sbd <viet.thanh1191@gmail.com>
2026-03-03 11:57:21 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -121,9 +121,13 @@ function startGame()
 
    //start up our loops
    var updaterate = 1000.0 / 60.0 ; //60 times a second
-   loopGameloop = setInterval(gameloop, updaterate);
    loopPipeloop = setInterval(updatePipes, 1400);
-
+   function smoothGameLoop(){
+      gameloop();
+      requestAnimationFrame(smoothGameLoop);
+   }
+   
+   requestAnimationFrame(smoothGameLoop);
    //jump from the start!
    playerJump();
 }
